a74f790e93346a1da2c41c0be3f04d66c1fedf1f
Patryk <patrykokpara@hotmail.com>
2023-01-18 15:53:10 +0000

    Second test passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,7 @@ describe 'fizzbuzz' do
     it 'returns buzz when 5 is typed in' do
         expect(fizzbuzz(5)).to eq 'buzz'
     end
+    it 'returns FizzBuzz if number is divisable by 3 and 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
 end 
