a247a3f4d88e6ff278feddabc5bfbe3d10629f32
Patryk <patrykokpara@hotmail.com>
2023-01-18 15:44:46 +0000

    First test passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,4 +5,7 @@ describe 'fizzbuzz' do
     it 'returns fizz when 3 is typed in' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
-end
+    it 'returns buzz when 5 is typed in' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+end 
